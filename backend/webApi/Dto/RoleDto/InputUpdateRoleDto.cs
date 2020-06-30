4c341434fdbc51b9f5d5b6bb29f2f0c8ec5007e4
Smoothiesss <ngquoccuong0498@gmail.com>
2020-06-30 17:50:42 +0700

    Update RoleController

--- backend/webApi/Dto/RoleDto/InputUpdateRoleDto.cs
+++ backend/webApi/Dto/RoleDto/InputUpdateRoleDto.cs
@@ -5,7 +5,7 @@ namespace ConstructionApp.Dto.RoleDto
 {
     public class InputUpdateRoleDto
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string ConcurrencyStamp { get; set; }
         public string NormalizedName { get; set; }
