22dd7434100cb3cfa9760693edfc511ca1775388
hai.do <thanhhaictld@gmail.com>
2020-07-01 22:21:09 +0700

    add landing page v2

---
+++ landingPage2/landing-page/src/homepage/gallery.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+
+export class Gallery extends Component {
+  render() {
+    return (
+        <div id="portfolio" className="text-center">
+        <div className="container">
+          <div className="section-title">
+            <h2>Thư viện</h2>
+            <p>Hình ảnh thực tế ở các công trình chúng tôi đảm nhận.</p>
+          </div>
+          <div className="row">
+            <div className="portfolio-items">
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh13.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Cơ sở vật chất</h4>
+                    </div>
+                    <img src="img/portfolio/anh13.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh1.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Somerset Cam Ranh Bay</h4>
+                    </div>
+                    <img src="img/portfolio/anh1.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh2.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Somerset Cam Ranh Bay</h4>
+                    </div>
+                    <img src="img/portfolio/anh2.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>              
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh4.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Alma Paradise Resort</h4>
+                    </div>
+                    <img src="img/portfolio/anh4.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh5.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Cao đẳng nghề Nha Trang</h4>
+                    </div>
+                    <img src="img/portfolio/anh5.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh8.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Hud Building Nha Trang</h4>
+                    </div>
+                    <img src="img/portfolio/anh8l.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh9.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Beau Rivage Nha trang</h4>
+                    </div>
+                    <img src="img/portfolio/anh9.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh10.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Beau Rivage Nha trang</h4>
+                    </div>
+                    <img src="img/portfolio/anh10.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh12.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Alma HBC</h4>
+                    </div>
+                    <img src="img/portfolio/anh12.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg"> <a href="img/portfolio/anh3.jpg" title="" data-lightbox-gallery="gallery1">
+                    <div className="hover-text">
+                      <h4>Beau Rivage Nha Trang</h4>
+                    </div>
+                    <img src="img/portfolio/anh3.jpg" className="img-responsive" alt=""/> </a> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Gallery
