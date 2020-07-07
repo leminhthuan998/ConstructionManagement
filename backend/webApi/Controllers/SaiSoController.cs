57344445dee6e1e0881f6144cc5970b0806eb190
Smoothiesss <ngquoccuong0498@gmail.com>
2020-07-07 19:29:31 +0700

    Update SaiSoController.cs

--- backend/webApi/Controllers/SaiSoController.cs
+++ backend/webApi/Controllers/SaiSoController.cs
@@ -7,6 +7,8 @@ using ConstructionApp.Entity;
 using System.Threading.Tasks;
 using System.Net;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace ConstructionApp.Controllers
 {
@@ -36,6 +38,32 @@ namespace ConstructionApp.Controllers
                 item.ThongTinMeTron = thongTinMeTron;
                 newRs.Add(item);
             }
+
+            return Ok(ApiResponse<List<SaiSo>>.ApiOk(newRs));
+        }
+
+        [HttpPost("filter")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ApiResponse<List<SaiSo>>))]
+        public async Task<IActionResult> FilterAction(DateTime startDate, DateTime endDate, string macCode, string tenHopDong)
+        {
+            // var results = await _repository.ToListAsync();
+            var newRs = new List<SaiSo>();
+            var results = await _repository.Where(x => x.ThongTinMeTron.NgayTron > startDate && x.ThongTinMeTron.NgayTron < endDate).ToListAsync();
+            var find = results.Count();
+            if (find == 0)
+            {
+                return Ok(ApiResponse<ModelStateDictionary>.ApiError(ModelState));
+            }
+            else 
+            {
+                foreach(var item in results)
+                {
+                var thongTinMeTron = await _dbContext.Set<ThongTinMeTron>().FirstAsync(x => x.Id.Equals(item.ThongTinMeTronId) && x.MAC.MacCode.Equals(macCode) && x.HopDong.TenHopDong.Equals(tenHopDong));
+                item.ThongTinMeTron = thongTinMeTron;
+                newRs.Add(item);
+                }
+            }
+            
 
             return Ok(ApiResponse<List<SaiSo>>.ApiOk(newRs));
         }
